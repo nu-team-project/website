9cdc9ab4ae4de4e435ff63b46d56ca836fd7cc73
MatthewShaw2000 <34135569+MatthewShaw2000@users.noreply.github.com>
2023-02-22 16:42:58 +0000

    Home page
    Main home page of the website with the canvas. The model has been imported onto it and the spheres representing the sensors have been added.
    The Spheres have been linked in with the slider.

---
+++ src/Components/Sphere.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+/**
+ * Sphere
+ * 
+ * Generates the spheres which represents the sensors.
+ * 
+ * @author Matthew Shaw
+ * @param {*} props 
+ * @returns Sphere mesh
+ */
+export default function Sphere(props)
+{
+    const [colour, setColour] = useState(false);
+
+    useEffect(() =>
+    {
+        if(!props.paneState.isPaneOpen)
+        {
+            setColour(false);
+        }
+    }, [props])
+
+    const sliderOpen = () =>
+    {
+        setColour(!colour);
+        props.openSlider();
+    }
+
+    return(
+        <mesh visible userData={{ Sensor: props.data }} position={props.coords} onClick={sliderOpen}>
+            <sphereGeometry args={[0.5, 16, 16]} />
+            <meshStandardMaterial color={colour ? 'green' : 'red'} />
+        </mesh>
+    );
+}
