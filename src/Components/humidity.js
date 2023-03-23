357c5c971ff45ff266d3a9d68182f6cbe6109b84
Lindsey Cawthorne <51476181+xxLindseyxx@users.noreply.github.com>
2023-03-23 10:49:36 +0000

    Started fetch

--- src/Components/humidity.js
+++ src/Components/humidity.js
@@ -2,20 +2,48 @@
 import '../CSS/Humidity.css';
 
 //Imports
-import { useState} from "react";
 import { Data } from "../temp-graph-data/Data";
 import { CategoryScale } from "chart.js";
+import React, { useState, useEffect } from 'react';
 
 //Components
 import LineChart from "./LineChart";
 import Chart from "chart.js/auto";
 
+//Images 
+import waterDrop from '../images/water-drop.png';
+
+
 // I have wrote this in my component as an example. 
 Chart.register(CategoryScale);
 
-export default function Humidity()
+export default function Humidity(props)
 {
+  const [channel, setChannel] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect( () => {
+      fetch("https://api.thingspeak.com/channels/2048224/fields/1.json?api_key=WNBPHCR9UFKPAV6N&results=2")
+      .then(
+          (response) => response.json()
+      )
+      .then(
+          (json) => {
+              setChannel(json.data)
+              setLoading(false)
+              console.log(json)
+          }
+      )
+      .catch(
+          (e) => {
+              console.log(e.message)
+          }
+      )
+  },[]);
+
+  const currentHumidity = channel.map((value, key) =>  key={key}
+
+  );
 //Extract the data from the dataset and use the date as the horizontal axis.
     const [chartData, setChartData] = useState({
         labels: Data.map((data) => data.date), //Data is in the temp-graph-data file. 
@@ -33,9 +61,15 @@ export default function Humidity()
     ]
   });
  // On line 38 I am trying to return the name but its not working for some reason. 
-    return(
-        <div>
-            <LineChart chartName="Humidity" chartData={chartData} />
-        </div>
-    );
+ return(
+  <div>
+      <h3 className='current-level-text'>Current Humidity Level:</h3>
+      <div className='wrapper'>
+          <img className='water-image' src={waterDrop} alt="Water-Drop" />
+          <span className='text'>{props.data}%</span>
+      </div>
+      <LineChart className='humidity-chart' chartName="Humidity" chartData={chartData}/>
+
+  </div>
+  );
 }
